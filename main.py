15a06dca7ad11aae0a31f7b6ca12b241c29b13ec
Matheus Motta Miranda de Mattos <109290214+mmmmattos13@users.noreply.github.com>
2023-04-14 15:18:34 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+sp = 67836.43
+rj = 36678.66
+mg = 29229.88
+es = 27165.48
+outros = 19849.53
+
+soma = sp + rj + mg + es + outros 
+print("São Paulo: {} %".format(sp/soma*100))
+print("Rio de Janeiro: {} %".format(mg/soma*100))
+print("Minas Gerais: {} %".format(mg/soma*100))
+print("Espirito Santo: {} %".format(es/soma*100))
+print("Outros: {} %".format(outros/soma*100))
